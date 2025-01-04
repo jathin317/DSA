7d69b8dfd30669389decaa493f513b0fecb52cd9
Jathin <jathin317@gmail.com>
2025-01-04 15:37:49 +0530

    addition fixed using stack

--- addition.cpp
+++ addition.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
+
 int main()
 {
     int a[] = {1, 2, 3};
     int b[] = {3, 2, 1};
     int sum[10];
-    int topa = sizeof(a) / sizeof(a[0]);
-    int topb = sizeof(b) / sizeof(b[0]);
+    int topa = sizeof(a) / sizeof(a[0]) - 1;
+    int topb = sizeof(b) / sizeof(b[0]) - 1;
     int tops = 0;
-    int carry;
-    for (int i = 0; topa == 0 && topb == 0; i++)
+    int carry = 0;
+
+    while (topa >= 0 || topb >= 0 || carry)
     {
-        int sum=
+        int x = (topa >= 0) ? a[topa--] : 0;
+        int y = (topb >= 0) ? b[topb--] : 0;
+        int tempSum = x + y + carry;
+        sum[tops++] = tempSum % 10;
+        carry = tempSum / 10;
     }
+
+    cout << "Sum of two numbers is: ";
+    for (int i = tops - 1; i >= 0; i--)
+    {
+        cout << sum[i];
+    }
+    cout << endl;
+
+    return 0;
 }
